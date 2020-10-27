e95300e5e378190cb0aeb4d87faffeaf3f54ea44
Sharadha Iyer <iyer.s.sharadha@gmail.com>
2020-10-27 15:02:26 +0530

    basic code

---
+++ cleverscoop.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  {
+          int n;
+    cin >> n;
+    ll a[n];
+        ll mx=0;
+    for(int i=0; i<n; i++)
+    {
+    cin >> a[i];
+    mx = max(mx, a[i]);
+    }
+    ll temp[1005]={0};
+  for(int i=0; i<n; i++)
+    {    temp[a[i]]++;
+    }
+    ll ans = 0;
+    // for(int i=0; i<n; i++)
+    // {
+    //   cout << temp[a[i]] << " ";
+// }
+    // cout << endl;
+    ll a1 = 0, a2 = 0;    for(ll i=1; i<=1000; i++)
+    {
+        for(ll j=i; j<=1000; j+=i)
+        {
+            if(i==j)
+            {
+              ans += ((temp[i]-1)*(temp[i]))/2;
+            }
+            if(i!=j)
+            {
+              ans += temp[i]*temp[j];
+            }
+        }
+    }
+    for(ll i=1; i<=1000; i++)
+    {
+        for(ll j=i; j<=1000; j+=i)
+        {
+            if(i==j)
+            {
+        ans += ((temp[i]-1)*(temp[i]))/2;
+            }
+        }
+    }
+    cout << ans << endl;
+    // cout << a2 << endl;
+  }
+  return 0;
+}
